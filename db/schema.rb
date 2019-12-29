fc0af4630e85fda43278a0e9564497b42af7cdb1
hamityay <hamityay@hotmail.com>
2019-12-29 14:19:59 +0300

    Generate list.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_29_111804) do
+ActiveRecord::Schema.define(version: 2019_12_29_111913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,14 @@ ActiveRecord::Schema.define(version: 2019_12_29_111804) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "lists", force: :cascade do |t|
+    t.string "description"
+    t.boolean "completed"
+    t.bigint "card_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["card_id"], name: "index_lists_on_card_id"
+  end
+
+  add_foreign_key "lists", "cards"
 end
