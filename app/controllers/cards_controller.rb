9ad9081d13c6a8985846892a1b69da591c13ae45
hamityay <hamityay@hotmail.com>
2019-12-29 19:32:07 +0300

    Update controllers to handle api cals.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,18 @@
 class CardsController < ApplicationController
+
+  def index
+    cards = Card.all
+    render json: cards
+  end
+
+  def create
+    card = Card.create(card_params)
+    render json: card
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:title)
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,24 @@
 class ListsController < ApplicationController
+
+  def index
+    lists = List.all
+    render json: lists
+  end
+
+  def create
+    list = List.create(list_params)
+    render json: list
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    render json: list
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:description, :completed, :card_id)
+  end
 end
